c54f662d43ac4261478ee5949c3fa0ce22fca6da
husamXA1 <husamxm0@gmail.com>
2024-12-14 21:55:31 +0200

    Added a dataclass for Expense

--- expense-tracker.py
+++ expense-tracker.py
@@ -1,2 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
+class Expense:
+  id: int
+  description: str
+  amount: int
+  createdAt: int
+  modifiedAt: int
+
 if __name__ == "__main__":
   print("Welcome to expense tracker CLI")
